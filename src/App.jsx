e59696c81b6cc6767d9ac184310a498926017500
fran <fran-valero@hotmail.com>
2023-12-14 14:40:02 -0300

    scroll-to-top

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,20 @@ import { FeaturedSection } from './components/Featured/featured';
 import { Route, Routes } from 'react-router-dom';
 import { ProductsPage } from './pages/products/productsPage';
 import { ContactSection } from './pages/contact/ContactSection';
+//funcion para que scrollee hacia arriba cuando cambiamos de pagina
+import ScrollToTop from './components/scrollTop/scrollTop';
 
 function App() {
   return (
     <>
       <HeaderNav/>
-      <Routes>
-        <Route path="/" element={<SecondHero />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/contact" element={<ContactSection />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<SecondHero />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/contact" element={<ContactSection />} />
+        </Routes>
+      </ScrollToTop>
       <FooterSection/>
     </>
   )
